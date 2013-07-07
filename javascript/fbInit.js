9a8b78f59f77e7215e63bc3f6a2741d4b4a40699
gtsui <starterssg@gmail.com>
2013-07-07 21:00:05 +0800

    first commit

---
+++ javascript/fbInit.js
@@ -0,0 +1,65 @@
+  window.fbAsyncInit = function() {
+  FB.init({
+    appId      : '166384400201584', // App ID
+    channelUrl : '//www.starters-singapore.com/channel.html', // Channel File
+    status     : true, // check login status
+    cookie     : true, // enable cookies to allow the server to access the session
+    xfbml      : true  // parse XFBML
+  });
+  };
+
+
+function login(){
+		document.getElementById('dimmer').style.display='block';
+    FB.login(function(response) {
+        if (response.authResponse) {
+            getInfo();
+        } else {
+            window.location = "http://www.starters-singapore.com";
+        }
+    }, {scope: 'email,user_education_history,user_work_history'}); //permissions
+}
+
+  // Load the SDK asynchronously
+  (function(d){
+   var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+   if (d.getElementById(id)) {return;}
+   js = d.createElement('script'); js.id = id; js.async = true;
+   js.src = "//connect.facebook.net/en_US/all.js";
+   ref.parentNode.insertBefore(js, ref);
+  }(document));
+
+function getInfo() {
+  FB.api('/me', function(response) {
+	postInfo(response);
+  });
+}
+
+//posts information from hidden form
+function postInfo(response) {
+	alert('Thanks for signing up! We\'ll be in touch with you soon');
+	document.getElementById('dimmer').style.display='none';
+
+	if(document.getElementById('email1').value === ""){
+		document.getElementById('email').value = response.email;
+	}else{
+		document.getElementById('email').value = document.getElementById('email1').value;
+	}
+	document.getElementById('name').value = response.name;
+	document.getElementById('firstname').value = response.first_name;
+	document.getElementById('gender').value = response.gender;
+	document.getElementById('mobile').value = document.getElementById('mobile1').value;
+	document.getElementById('referral').value = document.getElementById('referral1').value;
+
+	if(response.education){
+		document.getElementById('school').value = response.education.toSource();
+	}
+
+	if(response.work){
+		document.getElementById('work').value = response.work.toSource();
+	}
+
+	document.getElementById('full').value = response.toSource();
+	document.getElementById("fbInfo").submit();
+}
+
--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 var express = require("express");
-var app = express();
-app.use(express.logger());
-
 var fs = require("fs");
+
+var app = express();
+app.use('/css', express.static(__dirname + '/css'));
+app.use('/img', express.static(__dirname + '/img'));
+app.use('/javascript', express.static(__dirname + '/javascript'));
+
 var buf = fs.readFileSync("html/index.html");
 var index = buf.toString();
 
 app.get('/', function(request, response) {
-  response.send(index);
+    response.send(index);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+    console.log("Listening on " + port);
 });
