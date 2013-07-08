34737609796d8bb86d742baadba51a9053d93071
gtsui <starterssg@gmail.com>
2013-07-08 23:34:58 +0800

    export sendEmail module

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var fs = require("fs");
-var nodemailer = require("nodemailer");
+var clientEmail = require("./sendEmail.js");
 
 var app = express();
 app.use('/css', express.static(__dirname + '/css'));
@@ -10,29 +10,6 @@ app.use(express.bodyParser());
 
 var buf = fs.readFileSync("html/index.html");
 var index = buf.toString();
-
-function sendEmail(firstname, email){
-    var smtpTransport = nodemailer.createTransport("SMTP",{
-	service: "Gmail",
-	auth: {
-	    user: "starterssg@gmail.com",
-	    pass: "deutschebank"
-	}
-    });
-
-    smtpTransport.sendMail({
-	from: "Geoffrey at Starters <geoffrey@starters-singapore.com>",
-	to: email,
-	subject: "Kick off your Starter",
-	text: "Hi " + firstname + ",\n\nThanks for signing up with us for a Starter. We're searching high and low for people who we think you and your friends will enjoy kicking the night off with, so please bear with us while we work our mojo. We'll contact you as soon as everything is set up. Thanks for signing up " + firstname + ", and please feel free to reach out to me personally if there's anything on your mind. Enjoy!  \n\nRegards,\nGeoffrey Tsui\nStarters Singapore\ngeoffrey@starters-singapore.com\n+65 91759342",
-    }, function(error, response){
-	if(error){
-	    console.log(error);
-	} else{
-	    console.log("Success!");
-	}
-    });
-}
 
 app.get('/', function(request, response) {
     response.send(index);
@@ -49,8 +26,8 @@ app.post('/', function(request, response){
     var work = request.body.work;
     var full = request.body.full;
 
-    sendEmail(firstname,email);
-    response.send(index);
+    clientEmail.sendEmail(firstname,email);
+    response.redirect('/');
 });
 
 var port = process.env.PORT || 5000;
