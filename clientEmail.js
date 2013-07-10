30483b59186d030b05eaf32ccaf9ccfc1e3715fd
gtsui <starterssg@gmail.com>
2013-07-11 00:16:26 +0800

    test iphone email

--- clientEmail.js
+++ clientEmail.js
@@ -10,7 +10,7 @@ function sendEmail(firstname, email){
     });
     
     smtpTransport.sendMail({
-	from: "Geoffrey at Starters <geoffrey@starters-singapore.com>",
+	from: "Geoffrey at Starters <starterssg@gmail.com>",
 	to: email,
 	subject: "Kick off your Starter",
 	text: "Hi " + firstname + ",\n\nThanks for signing up with us for a Starter. We're searching high and low for people who we think you and your friends will enjoy kicking the night off with, so please bear with us while we work our mojo. We'll contact you as soon as everything is set up. Thanks for signing up " + firstname + ", and please feel free to reach out to me personally if there's anything on your mind. Enjoy! \n\nRegards,\nGeoffrey Tsui\nStarters Singapore\ngeoffrey@starters-singapore.com\n+65 91759342",
