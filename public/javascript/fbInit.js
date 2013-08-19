1d9042a2bd15c892a8ff780e524bdf050afdd72c
gtsui <starterssg@gmail.com>
2013-08-19 22:53:54 +0800

    rest api

--- public/javascript/fbInit.js
+++ public/javascript/fbInit.js
@@ -1,4 +1,15 @@
 $(function(){
+
+    var mongoApiKey = $("#mongoApiKey").attr("data-mongoApiKey");    
+    
+    alert(mongoApiKey);
+
+    $.ajax( { url: "https://api.mongolab.com/api/1/databases/starters/collections/users?apiKey=In29fC5VPy_T4HyNQ2t7nwgvRvv8J0Kf",
+              data: JSON.stringify({"test":"hello"}),
+              type: "POST",
+              contentType: "application/json" } );
+
+
     
     $(".popup").click(function(){
 	$(".signUpWrapper").show();
@@ -10,39 +21,6 @@ $(function(){
 	$(".signUpWrapper").hide();
     });
 
-    //posts information from hidden form
-    $.fn.postInfo = function(response) {
-	alert("Thanks for signing up! We'll be in touch with you soon.");
-
-	var mongoApiKey = $("#mongoApiKey").attr("data-mongoApiKey");    
-	
-	alert(mongoApiKey);
-	
-	$.ajax( { url: "https://api.mongolab.com/api/1/databases/starters/collections/users?apiKey=In29fC5VPy_T4HyNQ2t7nwgvRvv8J0Kf",
-		  data: JSON.stringify({"test":"heroku2"}),
-		  type: "POST",
-		  contentType: "application/json" } );
-	
-	if(document.getElementById('email1').value === ""){
-	    document.getElementById('email').value = response.email;
-	}else{
-	    document.getElementById('email').value = document.getElementById('email1').value;
-	}
-	document.getElementById('name').value = response.name;
-	document.getElementById('firstname').value = response.first_name;
-	document.getElementById('gender').value = response.gender;
-	document.getElementById('mobile').value = document.getElementById('mobile1').value;
-	document.getElementById('referral').value = document.getElementById('referral1').value;
-	
-	if(response.education){
-	    document.getElementById('school').value = JSON.stringify(response.education);
-	}
-	if(response.work){
-	    document.getElementById('work').value = JSON.stringify(response.work);
-	}
-	document.getElementById('full').value = JSON.stringify(response);
-	document.getElementById("fbInfo").submit();   
-    }
 });
 
 window.fbAsyncInit = function() {
@@ -78,7 +56,31 @@ function login(){
 function getInfo() {
     document.getElementById('email').value = document.getElementById('email1').value;
     FB.api('/me', function(response) {
-	$.postInfo(response);
+	postInfo(response);
     });
 }
 
+//posts information from hidden form
+function postInfo(response) {
+    alert("Thanks for signing up! We'll be in touch with you soon.");
+    
+    if(document.getElementById('email1').value === ""){
+	document.getElementById('email').value = response.email;
+    }else{
+	document.getElementById('email').value = document.getElementById('email1').value;
+    }
+    document.getElementById('name').value = response.name;
+    document.getElementById('firstname').value = response.first_name;
+    document.getElementById('gender').value = response.gender;
+    document.getElementById('mobile').value = document.getElementById('mobile1').value;
+    document.getElementById('referral').value = document.getElementById('referral1').value;
+    
+    if(response.education){
+	document.getElementById('school').value = JSON.stringify(response.education);
+    }
+    if(response.work){
+	document.getElementById('work').value = JSON.stringify(response.work);
+    }
+    document.getElementById('full').value = JSON.stringify(response);
+    document.getElementById("fbInfo").submit();   
+}
