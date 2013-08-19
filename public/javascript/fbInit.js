689416213a745211ff96d7ac5807a74c7f337b64
gtsui <starterssg@gmail.com>
2013-08-19 23:47:20 +0800

    rest api

--- public/javascript/fbInit.js
+++ public/javascript/fbInit.js
@@ -56,11 +56,10 @@ function getInfo() {
 //posts information from hidden form
 function postInfo(response) {
     
-    alert(JSON.stringify(response));
     var mongoApiKey = $("#mongoApiKey").attr("data-mongoApiKey");    
     
     $.ajax( { url: "https://api.mongolab.com/api/1/databases/starters/collections/users?apiKey=In29fC5VPy_T4HyNQ2t7nwgvRvv8J0Kf",
-              data: JSON.stringify({"this":"works!"}),
+              data: JSON.parse(response),
               type: "POST",
               contentType: "application/json" } );
     
