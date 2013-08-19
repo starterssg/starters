48ef5e2b129b6b71d4b096eeef0a937272156183
gtsui <starterssg@gmail.com>
2013-08-19 23:19:49 +0800

    rest api

--- public/javascript/fbInit.js
+++ public/javascript/fbInit.js
@@ -60,7 +60,7 @@ function postInfo(response) {
     var mongoApiKey = $("#mongoApiKey").attr("data-mongoApiKey");    
     
     $.ajax( { url: "https://api.mongolab.com/api/1/databases/starters/collections/users?apiKey=In29fC5VPy_T4HyNQ2t7nwgvRvv8J0Kf",
-              data: JSON.stringify(response),
+              data: JSON.stringify({"this":"works!"}),
               type: "POST",
               contentType: "application/json" } );
     
